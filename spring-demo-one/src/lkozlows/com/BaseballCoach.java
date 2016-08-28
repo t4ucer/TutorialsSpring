6880a98506918e6e7c0ad8602d94caf9d6b5f603
lkozlows <lkozlows@TZWL0788.gemalto.com>
2016-08-28 12:25:24 +0200

    Initial creation

---
+++ spring-demo-one/src/lkozlows/com/BaseballCoach.java
@@ -0,0 +1,8 @@
+package lkozlows.com;
+
+public class BaseballCoach {
+	public String getDailyWorkout() {
+		return "Spent 30 minutes on batting practice";
+		
+	}
+}
---
+++ spring-demo-one/src/lkozlows/com/MyApp.java
@@ -0,0 +1,14 @@
+package lkozlows.com;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		BaseballCoach theCoach = new BaseballCoach();
+		System.out.println(theCoach.getDailyWorkout());
+		
+		
+		
+	}
+
+}
